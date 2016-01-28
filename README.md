764cb77c8319aecb2232a620c6578a84f13bb139
Abigail Watson <awatson1978@gmail.com>
2016-01-27 21:05:55 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## clinical:active-layout-reset
+
+CSS reset for apps using clinical:active-layout.
+
+
+==========================
+####Package Installation  
+
+````bash
+meteor add clinical:active-layout-reset
+````
+
+==========================
+####Contributions & Licensing  
+
+![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)
---
+++ package.js
@@ -0,0 +1,29 @@
+Package.describe({
+  name: 'clinical:active-layout-reset',
+  version: '0.0.1',
+  summary: 'CSS reset for apps using clinical:active-layout',
+  git: 'https://github.com/clinical-meteor/active-layout-reset',
+  documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.1.0.3');
+
+  api.use('meteor-platform');
+  api.use('clinical:active-layout@0.7.5');
+  api.use('grove:less@0.1.1');
+
+  api.imply('clinical:active-layout');
+
+  api.addFiles('client/reset.less', 'client');
+  api.addFiles('client/typography.less', 'client');
+  api.addFiles('client/base.less', 'client');
+  api.addFiles('client/cards.less', 'client');
+  api.addFiles('client/forms.less', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('clinical:active-layout-reset');
+  api.addFiles('active-layout-reset-tests.js');
+});
