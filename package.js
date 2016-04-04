909e5fa4cb55780eea0e9251ddff13a0c7983f19
Abigail Watson <awatson1978@gmail.com>
2016-04-03 23:02:56 -0700

    bump and publish

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-layout-reset',
-  version: '0.2.0',
+  version: '0.2.1',
   summary: 'CSS reset for apps using clinical:active-layout',
   git: 'https://github.com/clinical-meteor/active-layout-reset',
   documentation: 'README.md'
@@ -10,7 +10,7 @@ Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
   api.use('meteor-platform');
-  api.use('clinical:active-layout@0.7.5');
+  api.use('clinical:active-layout@0.7.50');
   api.use('grove:less@0.1.1');
 
   api.imply('clinical:active-layout');
